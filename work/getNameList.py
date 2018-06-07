e00bbadbdfa5046f0d302d218307d3eb7a4e57c1
BinZhu <30952106+binzh93@users.noreply.github.com>
2018-06-07 09:09:28 +0800

    Create getNameList.py

---
+++ work/getNameList.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import json
+import os
+import re
+
+os.chdir("/Users/bin/Desktop/")
+
+file1 = os.path.join("json", "中纪委_gamma_0.0.1-resume_conv.json")
+file2 = os.path.join("json", "中纪委_gamma_0.0.1-resume_conv_2.json")
+file3 = os.path.join("json", "zjsjw_aaa-resume_conv.json")
+file4 = os.path.join("json", "zjsjw_aaa-resume_conv_2.json")
+
+def load1(file):
+    a = []
+    invalid_nums = 0
+    invalid_str = ''
+    with open(file, 'r') as f:
+        for line in f:
+            #print type(line)
+            #print eval(line.strip())
+            #name = eval(line.strip())['result']['resume'].split(' ')[0].split('简历')[0]
+            #print(line.strip())
+            name = eval(line.strip())['result']['resume']
+            #name = re.split(r"\s|:\s|:\s\s|：|：\s\s|：\s|,\s|:\n", name)
+            #print name
+            name = re.split(r"\s|:\s|:\s|：|：\s|,\s|:", name)
+            temp = name[0].split("个人简历")
+            temp = temp[0].split("简历")
+            #if len(temp)==0 | len(temp)>3:
+            #print name[1]
+            if len(temp) == 0:
+                temp = name[1].split("，")[0]
+            #print type(temp)
+            if len(temp[0]) == 0:
+                invalid_nums += 1
+                invalid_str += line.strip()
+
+            a.append(temp)
+
+    #c = set()
+    name_list = []
+    for i in a:
+        if len(i[0]) >0:
+            name_list.append(i[0])
+        if len(i[0]) == 0:
+            print '123424234234234234'
+        else:
+            print i[0]
+
+        #c.add(i[0])
+    print invalid_nums
+    print invalid_str
+    print len(a)
+    #print len(c)
+    a = [i[0] for i in a]
+    return name_list
+
+
+if __name__ == '__main__':
+    #load1(file2)
+    name_list = ['刘树琪', '刘树琪']
+    print name_list
+
